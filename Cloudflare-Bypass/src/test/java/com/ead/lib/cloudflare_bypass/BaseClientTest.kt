34ac28011ce465c43bf030fb3161afd0745604c5
darkryh <alex_torres-xc@hotmail.com>
2024-09-27 20:25:55 -0500

    junit tests for baseClient

--- Cloudflare-Bypass/src/test/java/com/ead/lib/cloudflare_bypass/BaseClientTest.kt
+++ Cloudflare-Bypass/src/test/java/com/ead/lib/cloudflare_bypass/BaseClientTest.kt
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.lang.reflect.Field
 
+@Suppress("DEPRECATION")
 @RunWith(RobolectricTestRunner::class)
 class BaseClientTest {
 
